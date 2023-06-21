9f096d2953609995267d5785af1baee8030d6dc6
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2023-06-22 00:39:18 +0530

    Update user_parse.js

--- user_parse.js
+++ user_parse.js
@@ -4,12 +4,7 @@ var sleep = require('system-sleep');
 
 let project = [];
 let users = [];
-var project_list = ['d11-hack2023-team1','d11-hack2023-team2','d11-hack2023-team3','d11-hack2023-team4','d11-hack2023-team5','d11-hack2023-team6','d11-hack2023-team7','d11-hack2023-team8','d11-hack2023-team9','d11-hack2023-team10',
-'d11-hack2023-team11','d11-hack2023-team12','d11-hack2023-team13','d11-hack2023-team14','d11-hack2023-team15','d11-hack2023-team16','d11-hack2023-team17','d11-hack2023-team18','d11-hack2023-team19','d11-hack2023-team20',
-'d11-hack2023-team21','d11-hack2023-team22','d11-hack2023-team23','d11-hack2023-team24','d11-hack2023-team25','d11-hack2023-team26','d11-hack2023-team27','d11-hack2023-team28','d11-hack2023-team29','d11-hack2023-team30',
-'d11-hack2023-team31','d11-hack2023-team32','d11-hack2023-team33','d11-hack2023-team34','d11-hack2023-team35','d11-hack2023-team36','d11-hack2023-team37','d11-hack2023-team38','d11-hack2023-team39','d11-hack2023-team40',
-'d11-hack2023-team41','d11-hack2023-team42','d11-hack2023-team43','d11-hack2023-team44','d11-hack2023-team45','d11-hack2023-team46','d11-hack2023-team47','d11-hack2023-team48','d11-hack2023-team49','d11-hack2023-team50'
-]
+var project_list = ['hack2023-team1','hack2023-team2']
 
 fs.createReadStream("./user_mapping.csv")
   .pipe(parse({ delimiter: ",", from_line: 1 }))
