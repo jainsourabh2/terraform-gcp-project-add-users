6b99b42b0623e8efbd595079cabdb82245f8df27
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2023-06-22 01:04:03 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -11,7 +11,7 @@ do
    cp main.tf.base ${arr[0]}.tf
    sed -i "s/project-id/${arr[0]}/g" "${arr[0]}.tf"
    sed -i "s/<<users>>/${arr[1]}/g" "${arr[0]}.tf"
-   #terraform apply -auto-approve
+   terraform apply -auto-approve -target=${arr[0]}.tf
    rm -rf terraform.tfstate
    rm -rf terraform.tfstate.backup
 done < input.csv
